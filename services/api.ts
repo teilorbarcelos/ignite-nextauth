6c68396ce399e19a7ad3f2ae365d7500ff573a95
Teilor Souza Barcelos <tsb-@live.com>
2021-12-26 13:19:26 -0300

    axios requests queue working :tada:

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,14 @@
 import axios, { AxiosError } from "axios"
 import { parseCookies, setCookie } from "nookies"
 
+interface FailedRequestsQueueProps {
+  onSuccess: (token: string) => void
+  onFailure: (error: AxiosError) => void
+}
+
 let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue: FailedRequestsQueueProps[] = []
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -18,24 +25,54 @@ api.interceptors.response.use(
         cookies = parseCookies()
 
         const { 'myNextAuth.refreshToken': refreshToken } = cookies
+        const originalConfig = error.config
 
-        api.post('/refresh', {
-          refreshToken
-        }).then(response => {
-          const { token, refreshToken: newRefreshToken } = response.data
+        if (!isRefreshing) {
+          isRefreshing = true
+          api.post('/refresh', {
+            refreshToken
+          }).then(response => {
+            const { token, refreshToken: newRefreshToken } = response.data
 
-          setCookie(undefined, 'myNextAuth.token', token, {
-            maxAge: 60 * 60 * 24 * 30, // 30 days
-            path: '/'
+            setCookie(undefined, 'myNextAuth.token', token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: '/'
+            })
+
+            setCookie(undefined, 'myNextAuth.refreshToken', newRefreshToken, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: '/'
+            })
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            failedRequestsQueue.forEach(request => request.onSuccess(token))
+            failedRequestsQueue = []
+          }).catch(error => {
+            failedRequestsQueue.forEach(request => request.onFailure(error))
+            failedRequestsQueue = []
+          }).finally(() => {
+            isRefreshing = false
           })
+        }
 
-          setCookie(undefined, 'myNextAuth.refreshToken', newRefreshToken, {
-            maxAge: 60 * 60 * 24 * 30, // 30 days
-            path: '/'
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+
+              if (!originalConfig?.headers) {
+                return reject('No valid headers configuration!')
+              }
+
+              originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+              resolve(api(originalConfig))
+            },
+
+            onFailure: (error: AxiosError) => {
+              reject(error)
+            }
           })
-
-          api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-
         })
       } else {
 
