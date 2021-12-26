51ba46de15dc752b9a0daae8d775f3a4c52cd659
Teilor Souza Barcelos <tsb-@live.com>
2021-12-26 12:00:35 -0300

    refreshing jwt token

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,45 @@
-import axios from "axios"
-import { parseCookies } from "nookies"
+import axios, { AxiosError } from "axios"
+import { parseCookies, setCookie } from "nookies"
 
-const { 'myNextAuth.token': token } = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
-    Authorization: `Bearer ${token}`
+    Authorization: `Bearer ${cookies['myNextAuth.token']}`
   }
 })
+
+api.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies()
+
+        const { 'myNextAuth.refreshToken': refreshToken } = cookies
+
+        api.post('/refresh', {
+          refreshToken
+        }).then(response => {
+          const { token, refreshToken: newRefreshToken } = response.data
+
+          setCookie(undefined, 'myNextAuth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+
+          setCookie(undefined, 'myNextAuth.refreshToken', newRefreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+
+          api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+        })
+      } else {
+
+      }
+    }
+  }
+)
